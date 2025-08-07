48e3bc4e18312df04fc8870af0d283f1dca2e7b0
Jagata Ballava Gopal Krushna Das <gopaldas9439@gmail.com>
2025-08-07 23:46:55 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age: "))
+if (age >18):
+    print("you area adult")
+else:
+    print("you are not an adult")
+if (age <= 5):
+    print("you are a kid")
+else:
+    print("enter a valid age")
