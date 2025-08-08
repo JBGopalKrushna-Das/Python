32a19184aa4f3cde3b7c07628dff16181ceb1de0
Jagata Ballava Gopal Krushna Das <gopaldas9439@gmail.com>
2025-08-08 22:55:44 +0530

    Add files via upload

---
+++ exponention.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the value of a="))
+print("The value of a is", a)
+b=int(input("Enter the value of b="))
+print("the value of b is", b)
+print("exponention of above two numbers is", a**b) # this operator is used to calculate the power of a number
+# for example 2**3 = 2 raised to the power of 3 which is equal to 8
