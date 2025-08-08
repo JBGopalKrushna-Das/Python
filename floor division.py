e61f9f9fd9f6f2ec3f71bc1e21ca4bf3d06c9ba1
Jagata Ballava Gopal Krushna Das <gopaldas9439@gmail.com>
2025-08-08 22:40:07 +0530

    Add files via upload

---
+++ floor division.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+print("The value of a is:", a)
+print("The value of b is:", b) 
+print("floor division of a and b:", a//b) # this operator use for round off the result to the nearest whole number
+
